06b78f1c18cb511622130296cd7119f739d452a2
Ekansh Goel <ekanshgoel123@gmail.com>
2023-03-03 19:06:37 -0500

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function changeText() {
     var checkbox = document.getElementById("checkbox");
     var text = document.getElementById("text");
     if (checkbox.checked) {
-      text.innerHTML = "Oh okay , i was just finding my robo girl 🥺 👉👈    !!!";
+      text.innerHTML = "Oh okay , I was just finding my robo girl 🥺 👉👈    !!!";
     } else {
       text.innerHTML = "Are you human?";
     }
